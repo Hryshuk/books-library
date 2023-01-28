409905f97e425b613a4aa16b5b7a71ab73880852
Dmitry Hryshuk <hryshuk@tut.by>
2023-01-28 03:00:59 +0300

    added list, view, delete crud operations for books and authors

--- src/Controller/AuthorsController.php
+++ src/Controller/AuthorsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,5 +29,44 @@ class AuthorsController extends AbstractController
         $authors = $this->authorRepository->findAll();
 
         return $this->render('authors/list.html.twig', ['authors' => $authors]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_authors_view")
+     */
+    public function view($id): Response
+    {
+        $author = $this->authorRepository->find($id);
+
+        return $this->render('authors/view.html.twig', ['author' => $author]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="app_authors_edit")
+     */
+    public function edit($id): Response
+    {
+        $author = $this->authorRepository->find($id);
+        if (!$author) {
+            throw $this->createNotFoundException('No author found for id '.$id);
+        }
+
+        return $this->render('authors/edit.html.twig', ['author' => $author]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="app_authors_delete")
+     */
+    public function delete($id, EntityManagerInterface $em): Response
+    {
+        $author = $this->authorRepository->find($id);
+        if (!$author) {
+            throw $this->createNotFoundException('No author found for id '.$id);
+        }
+
+        $em->remove($author);
+        $em->flush();
+
+        return $this->redirectToRoute('app_authors_list');
     }
 }
--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -4,10 +4,14 @@ namespace App\Controller;
 
 use App\Repository\AuthorRepository;
 use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/books" )
+ */
 class BooksController extends AbstractController
 {
     private BookRepository $bookRepository;
@@ -17,10 +21,52 @@ class BooksController extends AbstractController
         $this->bookRepository = $bookRepository;
     }
 
+    /**
+     * @Route("/",  name="app_books_list")
+     */
     public function list(): Response
     {
         $books = $this->bookRepository->findAll();
 
         return $this->render('books/list.html.twig', ['books' => $books]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_books_view")
+     */
+    public function view($id): Response
+    {
+        $book = $this->bookRepository->find($id);
+
+        return $this->render('books/view.html.twig', ['book' => $book]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="app_books_edit")
+     */
+    public function edit($id): Response
+    {
+        $book = $this->bookRepository->find($id);
+        if (!$book) {
+            throw $this->createNotFoundException('No book found for id '.$id);
+        }
+
+        return $this->render('books/edit.html.twig', ['book' => $book]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="app_books_delete")
+     */
+    public function delete($id, EntityManagerInterface $em): Response
+    {
+        $book = $this->bookRepository->find($id);
+        if (!$book) {
+            throw $this->createNotFoundException('No book found for id '.$id);
+        }
+
+        $em->remove($book);
+        $em->flush();
+
+        return $this->redirectToRoute('app_books_list');
     }
 }
